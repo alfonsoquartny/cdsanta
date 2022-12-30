623e8d604086a91a26c1f80206cf18d8f826e867
emrecds <seyrelmeyen2@outlook.com>
2022-12-30 17:51:34 +0300

    fixes

--- cdsanta/Assets/Scripts/Clouds.cs
+++ cdsanta/Assets/Scripts/Clouds.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Clouds : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public bool right;
+    Rigidbody2D m_Rigidbody;
+   public float m_Speed;
+
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        //Set the speed of the GameObject
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (right == true)
+        {
+            m_Rigidbody.velocity = transform.right * m_Speed;
+
+        }
+        else
+        {
+            m_Rigidbody.velocity = transform.right * -m_Speed;
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform.name == "Ters")
+        {
+            right = !right;
+        }
     }
 }
